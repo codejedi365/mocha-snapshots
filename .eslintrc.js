70556b0c56508a6a6515691816e2d47fd7418730
codejedi365 <codejedi365@gmail.com>
2020-05-04 02:25:03 -0400

    update/config > Add babel, eslint, & prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: [ "node_modules/*" ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    parser: "babel-eslint",
+  },
+  rules: {
+    "prettier/prettier": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+  overrides: [
+    {
+      files: [
+        "**/tests/**/*.test.{j,t}s?(x)"
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+};
