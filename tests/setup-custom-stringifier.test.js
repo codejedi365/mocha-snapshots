0554846c727347f1400d25dcdfa0f38897bf0bad
codejedi365 <codejedi365@gmail.com>
2020-05-05 01:30:30 -0400

    update/config > Apply prettier linting to testcases

--- tests/setup-custom-stringifier.test.js
+++ tests/setup-custom-stringifier.test.js
@@ -1,25 +1,30 @@
-import {expect} from 'chai'
-import {setup} from '../src'
+import { expect } from "chai";
+import { setup } from "../src/setup";
 
 /**
- * This provides a test to represent a user providing a custom stringifier 
+ * This provides a test to represent a user providing a custom stringifier
  * like `flatted` for handling circular references.
  */
-describe('setup custom stringifier', () => {
-  it('should match snapshots with a custom stringify function', () => {
-    let spy = null
-    setup({
-      stringifyFunction: (value, replacer, space) => {
-        value.a = 'whatever'
-        spy = value
-        return JSON.stringify(value, replacer, space)
-      }
-    })
+describe("setup custom stringifier", () => {
+    it("should match snapshots with a custom stringify function", () => {
+        let spy = null;
+        setup({
+            stringifyFunction: (value, replacer, space) => {
+                value.a = "whatever";
+                spy = value;
+                return JSON.stringify(value, replacer, space);
+            },
+        });
 
-    const obj = {test: 'one', 'a': () => {return 0} }
-    expect(obj).to.matchSnapshot()
-    expect(spy).to.not.equal(obj) // Due to normalize copy.
-    obj.a = 'whatever'
-    expect(spy).to.deep.equal(obj)
-  })
-})
+        const obj = {
+            test: "one",
+            a: () => {
+                return 0;
+            },
+        };
+        expect(obj).to.matchSnapshot();
+        expect(spy).to.not.equal(obj); // Due to normalize copy.
+        obj.a = "whatever";
+        expect(spy).to.deep.equal(obj);
+    });
+});
