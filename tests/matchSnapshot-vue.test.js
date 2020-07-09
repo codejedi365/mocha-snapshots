e7e829c9c614848304d9f2c9af34c6d9c44865ba
codejedi365 <codejedi365@gmail.com>
2020-07-09 00:48:10 -0500

    feature/vuejs-support > Implement specific Vue.js test cases

--- tests/matchSnapshot-vue.test.js
+++ tests/matchSnapshot-vue.test.js
@@ -1,59 +1,90 @@
-import React from "react";
-import Adapter from "enzyme-adapter-react-16";
-import Enzyme, { shallow } from "enzyme";
+/**
+ *
+ */
+import { mount, shallowMount } from "@vue/test-utils";
 import { expect } from "chai";
 import { setup } from "../src";
 
-Enzyme.configure({ adapter: new Adapter() });
+// the beautify should be handled by matchsnapshot code
+// expect(beautify(wrapper.html())).toMatchSnapshot(this);
 
-function MyReactComponent() {
-    return (
-        <div className="abc-123-45">
-          Lorem Ipsum dolor
+function MyVueComponent() {
+    return {
+        template: `<div>
+            <span>Lorem Ipsum dolor</span>
         </div>
-    )
+        `,
+    };
 }
 
-function MyReactComponentWithSpecialCharacters({ argument }) {
-    return (
-        <div className="abc-123-45" argument={argument}>
-          Lorem Ipsum dolor {argument} ""
+function MyVueComponentWithMethods() {
+    return {
+        template: `<div>
+            <span class="count">{{ count }}</span>
+            <button @click="increment">Increment</button>
         </div>
-    )
+        `,
+        data() {
+            return {
+                count: 0,
+            };
+        },
+        methods: {
+            increment() {
+                this.count++;
+            },
+        },
+    };
+}
+
+function MyVueComponentWithParams() {
+    return {
+        template: `<div>
+            <i>{{ message }}</i>
+        </div>
+        `,
+        props: {
+            message: String,
+        },
+    };
 }
 
 describe("matchSnapshot", () => {
-    describe("multiple tests with same it() title", () => {
-        describe("title one", () => {
-            it("should match snapshot", () => {
-                expect(123).to.matchSnapshot();
-            });
-        });
-
-        describe("title two", () => {
-            it("should match snapshot", () => {
-                expect(321).to.matchSnapshot();
-            });
-        });
-    });
-
-    it("should match snapshots", () => {
-        const wrapper = shallow(<MyReactComponent/>)
+    it("should match a mounted vue component", () => {
+        const wrapper = mount(MyVueComponent());
 
         expect(wrapper).to.matchSnapshot();
-        expect(123).to.matchSnapshot();
-        expect({ a: 1, b: { c: "lorem" } }).to.matchSnapshot();
+    });
+    it("should match a shallow mounted vue component", () => {
+        const wrapper = shallowMount(MyVueComponent());
+
+        expect(wrapper).to.matchSnapshot();
+    });
+    it("should match a mutated vue component", async () => {
+        const wrapper = mount(MyVueComponentWithMethods());
+        expect(wrapper).to.matchSnapshot(); // non-mutated
+
+        wrapper.vm.increment();
+        await wrapper.vm.$nextTick();
+        expect(wrapper).to.matchSnapshot(); // mutated to 1
+
+        wrapper.vm.increment();
+        await wrapper.vm.$nextTick();
+        expect(wrapper).to.matchSnapshot(); // mutated to 2
     });
 
     it("should match snapshots with double quotes", () => {
-        const wrapper = shallow(<MyReactComponentWithSpecialCharacters argument={'""'} />)
-
+        const wrapper = shallowMount(MyVueComponentWithParams(), {
+            propsData: {
+                message: 'program: "Hello World!"',
+            },
+        });
         expect(wrapper).to.matchSnapshot();
     });
 
     it("should match snapshots without classNames sanitization", () => {
         setup({ sanitizeClassNames: false });
-        const wrapper = shallow(<MyReactComponent/>)
+        const wrapper = mount(MyVueComponent());
         expect(wrapper).to.matchSnapshot();
     });
 });
